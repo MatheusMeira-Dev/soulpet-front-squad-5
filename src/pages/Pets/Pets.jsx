c55fb3cc2c52c036662a90e176df71a9805e7ac4
MatheusMeira-Dev <matheusmeira.psy@gmail.com>
2023-05-02 17:04:04 -0300

    Resolução de conflitos

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import "./Pets.css"
+import "./Pets.css";
 import { useEffect, useState } from "react";
 import { Table, Button, Modal, Pagination } from "react-bootstrap";
 import { Loader } from "../../components/Loader/Loader";
@@ -27,26 +27,26 @@ export function Pets() {
   }
 
   // Pagination pets
-  const [ petsPerPage, setPetPerPage ] = useState(10);
-  const [ currentPage, setCurrentPage ] = useState(0);
+  const [petsPerPage, setPetPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
   const startIndex = currentPage * petsPerPage;
   const endIndex = startIndex + petsPerPage;
-  const currentIndex = pets.slice(startIndex, endIndex)
-  const pages = Math.ceil( pets.length / petsPerPage)
+  const currentIndex = pets.slice(startIndex, endIndex);
+  const pages = Math.ceil(pets.length / petsPerPage);
 
   function prev() {
     if (currentPage > 0) {
-      setCurrentPage(currentPage - 1)
+      setCurrentPage(currentPage - 1);
     }
   }
 
   function next() {
     if (currentPage < pages - 1) {
-      setCurrentPage(currentPage + 1)
+      setCurrentPage(currentPage + 1);
     }
   }
   // Pagination pets
-  console.log(pages)
+  console.log(pages);
 
   const handleClose = () => {
     setPetId(null);
@@ -81,7 +81,7 @@ export function Pets() {
     <div className="pets container">
       <div className="d-flex justify-content-between align-items-center">
         <h1>Pets</h1>
-        <Button  >
+        <Button>
           <i className="bi bi-plus-lg me-2"></i> Pet
         </Button>
       </div>
@@ -107,13 +107,13 @@ export function Pets() {
                   <td>{pet.porte}</td>
                   <td>{pet.dataNasc}</td>
                   <td className="d-flex justify-content-center gap-2">
-                    <Button   onClick={() => handleShow(pet.id)}>
+                    <Button onClick={() => handleShow(pet.id)}>
                       <i className="bi bi-trash-fill"></i>
                     </Button>
-                    <Button   as={Link} to={`/pet/editar/${pet.id}`}>
+                    <Button as={Link} to={`/pet/editar/${pet.id}`}>
                       <i className="bi bi-pencil-fill"></i>
                     </Button>
-                    <Button   as={Link} to={`/petsinfo/${pet.id}`}>
+                    <Button as={Link} to={`/petsinfo/${pet.id}`}>
                       <i className="bi bi-info-lg"></i>
                     </Button>
                   </td>
@@ -124,13 +124,26 @@ export function Pets() {
         </Table>
       )}
       <Pagination className="d-flex justify-content-center">
-        <Button className="button rounded-start" variant="none" onClick={prev}><Pagination.Prev /></Button>
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
-        <Pagination.Item className="pagination">{currentPage + 1}</Pagination.Item>
-        <Pagination.Ellipsis />
-      <Pagination.Item>{pages}</Pagination.Item>
-        <Button className="button rounded-end" variant="none" onClick={next}><Pagination.Next /></Button>      
+        <Button className="button rounded-start px-2 mx-1" onClick={prev}>
+          <i class="bi bi-arrow-left-short"></i>
+        </Button>
+        <Pagination.Item>
+          {Array.from(Array(pages), (pets, index) => {
+            return (
+              <Button
+                className="mx-1"
+                style={index === currentPage ? { backgroundColor: "#3B2712" } : null }
+                value={index}
+                onClick={(e) => setCurrentPage(Number(e.target.value))}
+              >
+                {index + 1}
+              </Button>
+            );
+          })}
+        </Pagination.Item>
+        <Button className="button rounded-end px-2 mx-1" onClick={next}>
+          <i class="bi bi-arrow-right-short"></i>
+        </Button>
       </Pagination>
 
       <Modal show={show} onHide={handleClose}>
@@ -139,12 +152,8 @@ export function Pets() {
         </Modal.Header>
         <Modal.Body>Tem certeza que deseja excluir o Pet?</Modal.Body>
         <Modal.Footer>
-          <Button   onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button   onClick={onDelete}>
-            Excluir
-          </Button>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={onDelete}>Excluir</Button>
         </Modal.Footer>
       </Modal>
     </div>
