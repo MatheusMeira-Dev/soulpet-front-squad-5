bec52d21c6fcafc3c6e22345a5b97e857bda9ac5
Williana123 <willianavictoria31@gmail.com>
2023-05-04 08:07:24 -0300

    ok

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,145 +1,238 @@
-import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+import axios from "axios";
+import "./Pets.css";
+import { useEffect, useState } from "react";
+import { Table, Button, Modal, Pagination } from "react-bootstrap";
+import { Loader } from "../../components/Loader/Loader";
+import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
-export function AddPet() {
-  const [nome, setNome] = useState("");
-  const [tipo, setTipo] = useState("");
-  const [porte, setPorte] = useState("");
-  const [dataNasc, setDataNasc] = useState("");
-  const [nomeDono, setNomeDono] = useState("");
-  const [emailDono, setEmailDono] = useState("");
-  const [telefoneDono, setTelefoneDono] = useState("");
-  const [enderecoDono, setEnderecoDono] = useState("");
-  const history = useHistory();
+export function Pets() {
+  const [pets, setPets] = useState([]);
+  const [petId, setPetId] = useState(null);
+  const [show, setShow] = useState(null);
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  useEffect(() => {
+    initializeTable();
+  }, []);
 
-    // Validando os campos
-    if (
-      nome.trim() === "" ||
-      tipo.trim() === "" ||
-      porte.trim() === "" ||
-      dataNasc.trim() === "" ||
-      nomeDono.trim() === "" ||
-      emailDono.trim() === "" ||
-      telefoneDono.trim() === "" ||
-      enderecoDono.trim() === ""
-    ) {
-      alert("Por favor, preencha todos os campos");
-      return;
-    }
-
-    // Salvando os dados do pet
-    const newPet = {
-      nome,
-      tipo,
-      porte,
-      dataNasc,
-      dono: {
-        nome: nomeDono,
-        email: emailDono,
-        telefone: telefoneDono,
-        endereco: enderecoDono,
-      },
-    };
-
-    // Aqui você faria uma requisição para o backend para salvar os dados do novo pet
-
-    alert("Pet adicionado com sucesso!");
-
-    // Redirecionando para a tela de Pets após a inserção
-    history.push("/pets");
+  function initializeTable() {
+    axios
+      .get("http://localhost:3001/pets")
+      .then((response) => {
+        setPets(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
+  function NovoPet() {
+    const [pet, setPet] = useState({
+      nome: "",
+      especie: "",
+      idade: "",
+      raca: "",
+      peso: "",
+    });
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setPet((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // lógica para enviar as informações do Pet para o servidor
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nome">Nome:</label>
+        <input
+          type="text"
+          id="nome"
+          name="nome"
+          value={pet.nome}
+          onChange={handleChange}
+          required
+        />
+         <label htmlFor="especie">Espécie:</label>
+      <input
+        type="text"
+        id="especie"
+        name="especie"
+        value={pet.especie}
+        onChange={handleChange}
+        required
+      />
+          <label htmlFor="idade">Idade:</label>
+      <input
+        type="number"
+        id="idade"
+        name="idade"
+        value={pet.idade}
+        onChange={handleChange}
+        required
+      />
+          <label htmlFor="raca">Raça:</label>
+      <input
+        type="text"
+        id="raca"
+        name="raca"
+        value={pet.raca}
+        onChange={handleChange}
+        required
+      />
+         <label htmlFor="peso">Peso:</label>
+      <input
+        type="number"
+        id="peso"
+        name="peso"
+        value={pet.peso}
+        onChange={handleChange}
+        required
+      />
+
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
+export default NovoPet;
+  
+  // Pagination pets
+  const [petsPerPage, setPetPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
+  const startIndex = currentPage * petsPerPage;
+  const endIndex = startIndex + petsPerPage;
+  const currentIndex = pets.slice(startIndex, endIndex);
+  const pages = Math.ceil(pets.length / petsPerPage);
+
+  function prev() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function next() {
+    if (currentPage < pages - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  // Pagination pets
+  console.log(pages);
+
+  const handleClose = () => {
+    setPetId(null);
+    setShow(false);
+  };
+  const handleShow = (id) => {
+    setPetId(id);
+    setShow(true);
+  };
+
+  function onDelete() {
+    axios
+      .delete(`http://localhost:3001/pets/${petId}`)
+      .then((response) => {
+        toast.success(response.data.message, {
+          position: "bottom-right",
+          duration: 2000,
+        });
+        initializeTable();
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.response.data.message, {
+          position: "bottom-right",
+          duration: 2000,
+        });
+      });
+    handleClose();
+  }
+  
+
   return (
-    <div className="container">
-      <h1>Adicionar Pet</h1>
-      <Form onSubmit ={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formNome">
-          <Form.Label>Nome</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Digite o nome do pet"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-          />
-        </Form.Group>
+    <div className="pets container">
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Pets</h1>
+        <Button>
+          <i className="bi bi-plus-lg me-2"></i> Pet
+        </Button>
+      </div>
+      {pets === null ? (
+        <Loader />
+      ) : (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>tipo</th>
+              <th>porte</th>
+              <th>data de Nascimento</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentIndex.map((pet) => {
+              return (
+                <tr key={pet.id}>
+                  <td>{pet.nome}</td>
+                  <td>{pet.tipo}</td>
+                  <td>{pet.porte}</td>
+                  <td>{pet.dataNasc}</td>
+                  <td className="d-flex justify-content-center gap-2">
+                    <Button onClick={() => handleShow(pet.id)}>
+                      <i className="bi bi-trash-fill"></i>
+                    </Button>
+                    <Button as={Link} to={`/pet/editar/${pet.id}`}>
+                      <i className="bi bi-pencil-fill"></i>
+                    </Button>
+                    <Button as={Link} to={`/petsinfo/${pet.id}`}>
+                      <i className="bi bi-info-lg"></i>
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
+      <Pagination className="d-flex justify-content-center">
+        <Button className="button rounded-start px-2 mx-1" onClick={prev}>
+          <i class="bi bi-arrow-left-short"></i>
+        </Button>
+        <Pagination.Item>
+          {Array.from(Array(pages), (pets, index) => {
+            return (
+              <Button
+                className="mx-1"
+                style={index === currentPage ? { backgroundColor: "#3B2712" } : null }
+                value={index}
+                onClick={(e) => setCurrentPage(Number(e.target.value))}
+              >
+                {index + 1}
+              </Button>
+            );
+          })}
+        </Pagination.Item>
+        <Button className="button rounded-end px-2 mx-1" onClick={next}>
+          <i class="bi bi-arrow-right-short"></i>
+        </Button>
+      </Pagination>
 
-        <Form.Group className="mb-3" controlId="formTipo">
-          <Form.Label>Tipo</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Digite o tipo do pet"
-            value={tipo}
-            onChange={(e) => setTipo(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formPorte">
-          <Form.Label>Porte</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Digite o porte do pet"
-            value={porte}
-            onChange={(e) => setPorte(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formDataNasc">
-          <Form.Label>Data de Nascimento</Form.Label>
-          <Form.Control
-            type="date"
-            placeholder="Digite a data de nascimento do pet"
-            value={dataNasc}
-            onChange={(e) => setDataNasc(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formNomeDono">
-          <Form.Label>Nome do Dono</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Digite o nome do dono"
-            value={nomeDono}
-            onChange={(e) => setNomeDono(e.target.value)}
-            required
-          />
-        </Form.Group>    <Form.Group className="mb-3" controlId="formEmailDono">
-      <Form.Label>Email do Dono</Form.Label>
-      <Form.Control
-        type="email"
-        placeholder="Digite o email do dono"
-        value={emailDono}
-        onChange={(e) => setEmailDono(e.target.value)}
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-3" controlId="formTelefoneDono">
-      <Form.Label>Telefone do Dono</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Digite o telefone do dono"
-        value={telefoneDono}
-        onChange={(e) => setTelefoneDono(e.target.value)}
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-3" controlId="formEnderecoDono">
-      <Form.Label>Endereço do Dono</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Digite o endereço do dono"
-        value={enderecoDono}
-        onChange={(e) => setEnderecoDono(e.target.value)}
-      />
-    </Form.Group>
-
-    <Button variant="primary" type="submit">
-      Adicionar
-    </Button>
-  </Form>
-</div>
-);
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmação</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Tem certeza que deseja excluir o Pet?</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={onDelete}>Excluir</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
